9cddc7155693d8698a78705daa6a549ac402cf4a
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-15 11:28:03 +0530

    Initialize dependabot/npm_and_yarn/postcss-8.4.45

---
+++ src/app/[locale]/@loginDialog/(.)login/page.tsx
@@ -0,0 +1,5 @@
+import LoginModal from "~/components/layout/login-modal";
+
+export default async function Login() {
+  return <LoginModal />;
+}
---
+++ src/app/global-error.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import { Button } from "~/components/ui/button";
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex h-[calc(100vh-160px)] w-full flex-col items-center justify-center gap-y-4">
+      <h2 className=" text-5xl font-bold text-destructive">
+        Oops, Something Went Wrong!
+      </h2>
+      <Button onClick={() => reset()}>Try Again</Button>
+    </div>
+  );
+}
