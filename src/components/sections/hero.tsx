0a4d79dca9f44efbdad9694be8ea57fec97c64d4
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-16 16:44:29 +0530

    Update hero.tsx

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -20,10 +20,18 @@ export default async function Hero() {
     <section>
       <div className="container flex w-full flex-col items-center justify-center space-y-20 py-16 md:py-20 lg:py-24 xl:py-28">
         <div className="mx-auto w-full max-w-2xl ">
+          {/*<a
+            href="https://twitter.com/immoinulmoin/status/1661645764697919489"
+            title="Follow Updates"
+            target="_blank"
+            rel="noreferrer"
+            className="mx-auto mb-5 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors duration-300 hover:bg-blue-200"
+          >
+            <Icons.twitter className="h-5 w-5 text-blue-700" />
             <p className="text-sm font-semibold text-blue-700">
               {scopedT("top")} ChadNext
             </p>
-          </a>
+          </a>*/}
           <h1 className=" text-balance bg-gradient-to-br  from-gray-900 via-gray-800 to-gray-400 bg-clip-text text-center font-heading text-[40px] font-bold leading-tight tracking-[-0.02em] text-transparent  drop-shadow-sm duration-300 ease-linear [word-spacing:theme(spacing.1)] dark:bg-gradient-to-br dark:from-gray-100 dark:to-gray-900 md:text-7xl md:leading-[5rem]">
             {scopedT("main")}
           </h1>
